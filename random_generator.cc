19a14252fead75b6a8225e5af3e0ac5aa8bea239
ZeitHaum <zeithaum@qq.com>
2024-02-01 20:40:07 +0800

    add random_generator.cc

---
+++ random_generator.cc
@@ -0,0 +1,39 @@
+#include <random>
+#include <cstdint>
+#include "util/bitset.hh"
+#include <cassert>
+
+std::random_device rd;  // 随机数种子
+std::mt19937 gen(rd()); // 使用 Mersenne Twister 引擎
+
+// 生成 [l, r] 范围内的数据
+template <typename T>
+T getRandomInt(T l, T r){
+    assert(std::is_integral<T>::value);
+    std::uniform_int_distribution<T> dis(l, r); 
+    return dis(gen);
+}
+
+template <typename T>
+T getRandomFloating(T l, T r){
+    assert(std::__is_floating<T>::__value);
+    std::uniform_real_distribution<T> dis(l, r); 
+    return dis(gen);
+}
+
+void getRandomBitset(SimpleSSD::Bitset& ret){
+    for(uint32_t i = 0; i<ret.size(); ++i){
+        if(getRandomFloating<float>(0.0f, 1.0f) >= 0.5f){
+            ret.set(i);
+        }
+    }
+}
+
+template <typename T>
+void getRamdomVector(std::vector<T>& ret, T l, T r){
+    for(uint32_t i = 0; i<ret.size(); ++i){
+        ret[i] = (T)(getRandomInt(l, r));
+    }
+}
+
+
--- unit_test.cc
+++ unit_test.cc
@@ -4,8 +4,10 @@
 #include "ftl/common/block.hh"
 #undef private
 #include "gtest/gtest.h"
+#include "random_generator.cc"
 
 using namespace SimpleSSD::FTL;
+using namespace SimpleSSD;
 
 /**
  * The following tests are all base on my project Simplessd:
@@ -13,17 +15,55 @@ using namespace SimpleSSD::FTL;
  * Most code is in ftl/.
 */
 //begin Test blockStat, Related commit [dev 8d6c014] Finish blockStat
-TEST(FTLBlockTest, setStaticAttrTest){
-    EXPECT_EQ(Block::iounitSize, 0);
-    EXPECT_EQ(Block::iounitSize, 0);
-    Block::setStaticAttr(16, 8);
-    EXPECT_EQ(Block::iounitSize, 16);
-    EXPECT_EQ(Block::maxCompressedPageCount, 8);
-}
-
-// Block test_block = Block(0, 768, 16);
-// TEST(FTLBlockTest, updateStatWriteTest){
-//     test_block.blockstat.reset();
-//     EXPECT_EQ(Block::iounitSize, 16);
+// TEST(FTLBlockTest, setStaticAttrTest){
+//     EXPECT_EQ(Block::iounitSize, 0);
+//     EXPECT_EQ(Block::iounitSize, 0);
+//     Block::setStaticAttr(4096, 8);
+//     EXPECT_EQ(Block::iounitSize, 4096);
 //     EXPECT_EQ(Block::maxCompressedPageCount, 8);
 // }
+
+// TEST(FTLBlockTest, updateStatWriteTest){
+//     Block::setStaticAttr(4096, 8);
+//     Block uncomp_block = Block(0, 768, 16);
+//     Block comp_block = Block(0, 768, 16);
+//     uncomp_block.blockstat.reset();
+//     comp_block.blockstat.reset();
+//     //random generate samples
+//     Bitset validMask = Bitset(8);
+//     std::vector<uint32_t> lens(8, 0);
+//     /*
+//     * Uncompress Test
+//     */
+//     for(int i = 0; i<8; ++i){
+//         lens[i] = 4096;
+//     }
+//     BlockStat std_{768*16*4096, 768*16*4096, 768*16, 0,768*16};
+//     validMask.set(0);
+//     for(uint16_t i = 0; i<768*16; ++i){
+//         uncomp_block.updateStatWrite(lens, validMask);
+//     }
+//     EXPECT_EQ(std_, uncomp_block.blockstat);
+
+//     /*
+//     * compress Test
+//     */
+//     std_.reset();
+//     for(uint16_t i = 0; i<768 * 16; ++i){
+//         //both compress and uncompress
+//         getRandomBitset(validMask);
+//         getRamdomVector<uint32_t>(lens, 1, 4096);
+//         for(uint16_t j = 0; j<8; ++j){
+//             if(validMask.test(j)) {
+//                 ++std_.totalUnitCount;
+//                 std_.totalDataLength += 4096;
+//                 std_.validDataLength += lens[j];
+//                 if(lens[j] < 4096) ++std_.compressUnitCount;
+//             }
+//         }
+//         if(validMask.any()) ++std_.validIoUnitCount;
+//         comp_block.updateStatWrite(lens, validMask);
+//         EXPECT_EQ(std_, comp_block.blockstat);
+//     }
+// }
+
