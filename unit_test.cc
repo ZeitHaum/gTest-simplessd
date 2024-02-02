1e52302c4cac9cf6aa646f1c098f79d597f36805
ZeitHaum <zeithaum@qq.com>
2024-02-02 20:15:45 +0800

    Finish init simpleSSD

--- unit_test.cc
+++ unit_test.cc
@@ -1,13 +1,11 @@
 //author: ZeitHaum
-
-#define private public
-#include "ftl/common/block.hh"
-#undef private
+#include "ftl/page_mapping.hh"
 #include "gtest/gtest.h"
 #include "random_generator.cc"
 
 using namespace SimpleSSD::FTL;
-using namespace SimpleSSD;
+
+const std::string config_file = "./simplessd/config/samsung_983dct_1.92tb.cfg";
 
 /**
  * The following tests are all base on my project Simplessd:
@@ -15,55 +13,34 @@ using namespace SimpleSSD;
  * Most code is in ftl/.
 */
 //begin Test blockStat, Related commit [dev 8d6c014] Finish blockStat
-// TEST(FTLBlockTest, setStaticAttrTest){
-//     EXPECT_EQ(Block::iounitSize, 0);
-//     EXPECT_EQ(Block::iounitSize, 0);
-//     Block::setStaticAttr(4096, 8);
-//     EXPECT_EQ(Block::iounitSize, 4096);
-//     EXPECT_EQ(Block::maxCompressedPageCount, 8);
-// }
+class PageMappingTestFixture: public ::testing::Test{
+protected:
+    void SetUp() override
+    {
+        //called before every testsuit
+        conf = new ConfigReader();
+        if(!conf->init(config_file)){
+            assert(0 && "Failed to laod file.");
+        }
+        p_dram = new SimpleSSD::DRAM::SimpleDRAM(conf);
+        p_ftl = new FTL(conf, p_dram);
+    }
 
-// TEST(FTLBlockTest, updateStatWriteTest){
-//     Block::setStaticAttr(4096, 8);
-//     Block uncomp_block = Block(0, 768, 16);
-//     Block comp_block = Block(0, 768, 16);
-//     uncomp_block.blockstat.reset();
-//     comp_block.blockstat.reset();
-//     //random generate samples
-//     Bitset validMask = Bitset(8);
-//     std::vector<uint32_t> lens(8, 0);
-//     /*
-//     * Uncompress Test
-//     */
-//     for(int i = 0; i<8; ++i){
-//         lens[i] = 4096;
-//     }
-//     BlockStat std_{768*16*4096, 768*16*4096, 768*16, 0,768*16};
-//     validMask.set(0);
-//     for(uint16_t i = 0; i<768*16; ++i){
-//         uncomp_block.updateStatWrite(lens, validMask);
-//     }
-//     EXPECT_EQ(std_, uncomp_block.blockstat);
+    void TearDown() override
+    {
+        //called after every testsuit
+        delete conf;
+        delete pFtl;
+    }
 
-//     /*
-//     * compress Test
-//     */
-//     std_.reset();
-//     for(uint16_t i = 0; i<768 * 16; ++i){
-//         //both compress and uncompress
-//         getRandomBitset(validMask);
-//         getRamdomVector<uint32_t>(lens, 1, 4096);
-//         for(uint16_t j = 0; j<8; ++j){
-//             if(validMask.test(j)) {
-//                 ++std_.totalUnitCount;
-//                 std_.totalDataLength += 4096;
-//                 std_.validDataLength += lens[j];
-//                 if(lens[j] < 4096) ++std_.compressUnitCount;
-//             }
-//         }
-//         if(validMask.any()) ++std_.validIoUnitCount;
-//         comp_block.updateStatWrite(lens, validMask);
-//         EXPECT_EQ(std_, comp_block.blockstat);
-//     }
-// }
+    SimpleSSD::ConfigReader* conf;
+    FTL* p_ftl;
+    SimpleSSD::DRAM::AbstractDRAM * p_dram;
+};
+
+TEST_F(PageMappingTestFixture, InitTest){
+    //Samply Test SetUp Function(Initialize Test);
+    ASSERT_TRUE(true);
+}
+
 
